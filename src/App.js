709b5a7588ede298ab7a8ec4a2fbe4940711f090
xaviguasch <xaviguasch@gmail.com>
2021-12-06 15:40:52 +0100

    added initial API calls

--- src/App.js
+++ src/App.js
@@ -1,6 +1,33 @@
+import React, { useState, useEffect } from 'react'
+
 import './App.css'
 
 function App() {
+  const [weather, setWeather] = useState([])
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(successCallback, errorCallback)
+  }, [])
+
+  const getWeather = () => {
+    fetch(
+      'https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/search/?query=london'
+    )
+      .then((res) => res.json())
+      .then((data) => setWeather(data))
+  }
+
+  const successCallback = (position) => {
+    const lat = position.coords.latitude
+    const lon = position.coords.longitude
+
+    console.log(lat, lon)
+  }
+
+  const errorCallback = (error) => {
+    console.error(error)
+  }
+
   return (
     <div className='App'>
       <h1>Weather App</h1>
